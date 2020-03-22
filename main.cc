200ae7ea97513c7f273e725294152113c9c78330
amiremohamadi <amiremohamadi@yahoo.com>
2020-03-22 15:26:45 +0430

    update main.cc

--- main.cc
+++ main.cc
@@ -2,10 +2,13 @@
 #include <tuntap.hh>
 
 int main() {
-  try {
-    Iface iface("tun0", Mode::TUN);
-  } catch (const char *e) {
-    std::cout << e << std::endl;
+  Iface iface("tun0", Iface::Mode::TUN);
+  uint8_t bytes[1500];
+
+  while (1) {
+    size_t nbytes = iface.receive(bytes, 1500);
+    std::cout << nbytes << std::endl;
   }
+
   return 0;
 }
