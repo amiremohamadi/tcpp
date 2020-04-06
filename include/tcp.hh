cfb9b2640b02f24c7ec153cf9d29279b4e876956
amiremohamadi <amiremohamadi@yahoo.com>
2020-04-06 20:56:25 +0430

    add connection holder
    the connection structure stores important variabels of a connection

--- include/tcp.hh
+++ include/tcp.hh
@@ -2,6 +2,47 @@
 #define TCP_HH
 
 #include <map>
+
+struct connection {
+  enum State { CLOSED, LISTEN, SYNRCVD, ESTAB } state;
+  // keeps track of send sequence varuables
+  struct sendseq {
+
+    /*         1         2          3          4               */
+    /*    ----------|----------|----------|----------          */
+    /*           SND.UNA    SND.NXT    SND.UNA                 */
+    /*                                +SND.WND                 */
+    /*  1 - old sequence numbers which have been acknowledged  */
+    /*  2 - sequence numbers of unacknowledged data            */
+    /*  3 - sequence numbers allowed for new data transmission */
+    /*  4 - future sequence numbers which are not yet allowed  */
+    /*                    Send Sequence Space	                 */
+    size_t una;
+    size_t nxt;
+    size_t wnd;
+    bool up;
+    size_t wl1;
+    size_t wl2;
+    size_t iss;
+  } send;
+
+  // keeps track of receive sequence varuables
+  struct recvseq {
+    /*     1          2          3                            */
+    /*    ----------|----------|----------                    */
+    /*           RCV.NXT    RCV.NXT                           */
+    /*                     +RCV.WND                           */
+    /* 1 - old sequence numbers which have been acknowledged  */
+    /* 2 - sequence numbers allowed for new reception         */
+    /* 3 - future sequence numbers which are not yet allowed  */
+    /*                 Receive Sequence Space                 */
+    size_t nxt;
+    size_t wnd;
+    bool up;
+    size_t irs;
+  } recv;
+};
+
 class Tcp {
   // all tcp stuffs gonna hanle here
 public:
@@ -9,7 +50,6 @@ public:
   map<quad, connection> connections;
 
 private:
-  enum State { Closed, Listen, SynRcvd, Estab };
 };
 
 struct quad {
